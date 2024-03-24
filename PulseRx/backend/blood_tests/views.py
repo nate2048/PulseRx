9151aaccf0ad4e18ab8c816d8dd901613394f657
rudyb30 <73556466+rudyb30@users.noreply.github.com>
2024-03-24 11:43:57 -0400

    small edit

--- PulseRx/backend/blood_tests/views.py
+++ PulseRx/backend/blood_tests/views.py
@@ -35,8 +35,6 @@ class TestAndMarkerView(APIView):
     def get(self, request):
         selected_user = request.user
 
-<<<<<<< HEAD
-
         # Filter BloodMarker based on logged in user
         blood_markers = BloodMarker.objects.filter(blood_test__user=selected_user).select_related('blood_test')
 
@@ -46,13 +44,6 @@ class TestAndMarkerView(APIView):
         blood_markers = BloodMarker.objects.filter(blood_test__user=selected_user).select_related('blood_test')
 
 
-
-=======
-        # Filter BloodMarker based on logged in user
-        blood_markers = BloodMarker.objects.filter(blood_test__user=selected_user).select_related('blood_test')
-
-
->>>>>>> 9fd0f288 (markers)
         data = {}
 
         # Iterate through blood markers and populate the data dictionary
@@ -65,32 +56,17 @@ class TestAndMarkerView(APIView):
             }
 
 
-<<<<<<< HEAD
 
             if marker.name in data:
                 data[marker.name]['tests'].append(blood_test_info)
 
 
-
-            if marker.name in data:
-                data[marker.name]['tests'].append(blood_test_info)
-
-
-=======
-            if marker.name in data:
-                data[marker.name]['tests'].append(blood_test_info)
-
->>>>>>> 9fd0f288 (markers)
             else:
                 data[marker.name] = {
                     'tests': [blood_test_info]
                 }
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 9fd0f288 (markers)
         return Response(data)
 
 # def post(self, request):
